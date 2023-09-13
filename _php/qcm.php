e9377250e7024138fd67e59597d85886d4197020
BIYA Paul <77636493+bpsmartdesign@users.noreply.github.com>
2023-09-13 18:36:24 +0100

    qcm: answer the first 20 questions

--- _php/qcm.php
+++ _php/qcm.php
@@ -14,6 +14,8 @@
  * 
  * >> $x = true and false;
  * >> var_dump($x);
+ * 
+ * ** this can be converted to ($x = true) and false; so $x will be true.
  */
 
 /**** 3
@@ -37,6 +39,8 @@
  * >> $a = '1';
  * >> $b = &$a;
  * >> $b = "2$b";
+ * 
+ * ** $a will be '21' and $b will be '21' because $b is a reference to $a.
  */
 
 /**** 5
@@ -45,10 +49,16 @@
  * >> var_dump(0123 == 123);
  * >> var_dump('0123' == 123);
  * >> var_dump('0123' === 123);
+ * 
+ * ** the first one will be true because 0123 is an octal number and 123 is a decimal number.
+ * ** the second one will be true because '0123' will be converted to 123 due to loose comparison (==).
+ * ** the third one will be false because '0123' is a string and 123 is an integer.
  */
 
 /**** 6
  * What will $x be equal to after the statement $x = 3 + "15%" + "$25"?
+ * 
+ * ** $x will be 18 because '15%' will be converted to 15 and '$25' will be converted to 0.
  */
 
 /**** 7
@@ -56,6 +66,8 @@
  * 
  * >> $text = 'John ';
  * >> $text[10] = 'Doe';
+ * 
+ * ** $text will be 'John Doe' and strlen($text) will return 11.
  */
 
 /**** 8
@@ -64,18 +76,33 @@
  * $a = "PHP";
  * $a = $a + 1;
  * echo $a;
+ * 
+ * ** it will echo 1 because "PHP" is a string and it will be converted to 0.
  */
 
 /**** 9
- * What are Traits?
+ * What are Traits in PHP?
+ * 
+ * ** Traits are a mechanism for code that allows you to reuse code (functions) in classes
+ *    without taking care of inheritance.
  */
 
 /**** 10
  * What is PEAR in php?
+ * 
+ * ** PEAR (Php Extension and Application Repository) is a package manager for PHP just like composer.
  */
 
 /**** 11
  * What is use of the header() function in PHP?
+ * 
+ * ** The header() function is used to send a raw HTTP header to a client.
+ *    It must be called before sending the actual output, otherwise it will not work.
+ *    It is used to redirect a page to another location.
+ *    It is used to create HTTP response status.
+ *    It is used to set cookies.
+ *    It is used to protect the webpage from cache.
+ * 
  */
 
 /**** 12
@@ -87,38 +114,66 @@
  * >> }
  * 
  * What will be the value of $x after this code executes?
+ * 
+ * ** $x will be 255 because '0xFF' will be converted to 255.
  */
 
 /**** 13
  * What is the difference between include_once() and require_once()
+ * 
+ * ** include_once() check if the file has already been included before. 
+ *      If it has, it will not be included again.
+ *      if it has not been included before, it will be included.
+ *      It will return an alert without interrupts the script in case of failure.
+ *    require_once() works the same way as include_once() 
+ *      but it will return a fatal error in case of failure.
  */
 
 /**** 14
  * What is the difference between a session and cookies?
+ * 
+ * ** A cookie is stored in browser user while session is stored in server.
  */
 
 /**** 15
  * How can we get the IP address of the client?
+ * 
+ * ** $_SERVER['REMOTE_ADDR']
  */
 
 /**** 16
  * What’s the difference between unset() and unlink()?
+ * 
+ * ** unset() is used to destroy a variable while unlink() is used to delete a file.
  */
 
 /**** 17
  * What are the main error types in PHP and how do they differ?
+ * 
+ * ** There are 4 main error types in PHP:
+ *    1. Notices: These are trivial, non-critical errors that PHP encounters while executing a script.
+ *    2. Warnings: These are more serious errors that PHP encounters while executing a script.
+ *    3. Fatal: These are critical errors that halt the execution of a script.
+ *    4. Parse: These parse errors occur when PHP encounters something that it cannot parse.
  */
 
 /**** 18
  * What is the difference between GET and POST?
+ * 
+ * ** GET is used to request data from a specified resource while 
+ *    POST is used to send data to a server to create/update a resource.
  */
 
 /**** 19
  * How can you enable error reporting in PHP?
+ * 
+ * ** error_reporting(E_ALL);
  */
 
 /**** 20
  * Can you extend a Final defined class?
+ * 
+ * ** No, you can't.
  */
 
 /**** 21
